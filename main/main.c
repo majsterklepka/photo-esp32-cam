603c25b406a770a1ce7d1d62633e4109180b99da
Paweł Sobótka <48721262935pl@gmail.com>
2022-03-01 15:14:38 +0100

    include gnu project preambule into file

--- main/main.c
+++ main/main.c
@@ -1,6 +1,32 @@
 /**
- * This example takes a picture every 5s and print its size on serial monitor.
+ * @file main.c
+ *
+ * 	 @date wto, 1 mar 2022, 15:09:28 CET 
+ *    @license GNU GPLv3
+ *     @author Paweł Sobótka <48721262935pl@gmail.com>
+ *  @copyright © Paweł Sobótka, 2017-2022, all rights reserved
+ *        URL: https://github.com/majsterklepka/photo-esp23-cam
+ *    Company: mgr inż. Paweł Sobótka, self-employed, individual creator 
+ *    Address: POLAND, masovian, Szydłowiec, 26-500
+ * NIP(taxid): 799-169-51-12
+ *
+ * This file is part of camera-sd-card.
+ *
+ *   camera-sd-card is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   camera-sd-card is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with camera-sd-card.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 
 // =============================== SETUP ======================================
 
